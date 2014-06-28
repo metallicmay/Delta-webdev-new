25665dadfee66e2498a49219a714f20d699881bf
metallicmay <metallicmay@gmail.com>
2014-06-28 19:31:31 +0530

    Update photouploadform.php

--- photouploadform.php
+++ photouploadform.php
@@ -1,6 +1,6 @@
 <html>
 <form enctype="multipart/form-data" action="photoupload.php" method="post">
-<label for="rn"> Roll Number* </label> 
+	<label for="rn"> Roll Number* </label> 
 <input type="text" name="rollnumber" id="rn"><br></br>
  Select your photo: (File size not more than 2 MB) <br></br><input type="file" name="photo">
  
