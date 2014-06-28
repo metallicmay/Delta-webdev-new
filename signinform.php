3b58291ea88cf85af3b57d693d79d5ed20022d13
metallicmay <metallicmay@gmail.com>
2014-06-28 15:41:25 +0530

    Create signinform.php

---
+++ signinform.php
@@ -0,0 +1,15 @@
+<html>
+<form action="signin.php" method="post">
+   <table>
+   <tr>
+   <td> Roll number: </td> <td> <input type="text" name="rollnum"> </td>
+   </tr>
+   <tr>
+   <td> Password: </td> <td> <input type="password" name="pass"> </td>
+   </tr>
+   <tr>
+   <td> <input type="submit" value="Log in"> </td>
+   </tr>
+   </table>
+   </form>
+</html>
