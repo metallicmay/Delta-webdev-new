3b9e82e57c5c037c2f767ff758c2870f3c9ed798
metallicmay <metallicmay@gmail.com>
2014-06-28 18:34:38 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -5,8 +5,8 @@
 <body style="background-color: blue;color:white;">
 <h1 align=center> WELCOME TO DELTA! </h1>
 <div>
-<a href="signupform.php"> <input type="button" style="background-color:skyblue; color: white;" value="Sign up"></a>
-<br></br> <a href ="signinform.php"> <input type="button" style="background-color:skyblue; color: white;" value="Log in"></a>
+<a href="signupform.php"> <input type="button" style="background-color:skyblue; color: black;" value="Sign up"></a>
+<br></br> <a href ="signinform.php"> <input type="button" style="background-color:skyblue; color: black;" value="Log in"></a>
 </div>
 </body>
 </html>
