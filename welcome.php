f76bc63a2b7006d3f749ab8f1f7de81b19cfb576
metallicmay <metallicmay@gmail.com>
2014-06-28 15:37:34 +0530

    Update and rename landingpage.html to welcome.php

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> DELTA TASK TWO </title>
+</head>
+<body style="background-color: blue;color:white;">
+<h1 align=center> WELCOME TO DELTA! </h1>
+<div>
+<a href="signupform.php"> <input type="button" style="background-color:skyblue; color: white;" value="Sign up"></a>
+<br></br> <a href ="signinform.php"> <input type="button" style="background-color:skyblue; color: white;" value="Log in"></a>
+</div>
+</body>
+</html>
