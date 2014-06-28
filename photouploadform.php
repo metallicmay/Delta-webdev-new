8c4d5c0895298ce78bb3225faadd2cd35ad1b4f8
metallicmay <metallicmay@gmail.com>
2014-06-28 15:42:28 +0530

    Create photouploadform.php

---
+++ photouploadform.php
@@ -0,0 +1,9 @@
+<html>
+<form enctype="multipart/form-data" action="photoupload.php" method="post">
+ Select your photo: (File size not more than 2 MB) <input type="file" name="photo">
+ 
+<br></br> <input type="submit" name="picsubmit" value="Upload image" />
+ 
+</form>
+<a href=signin.php> Back to home page: <button> Back </button> </a>
+</html>
